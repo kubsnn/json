5251193afd5da83335023889ebc6f55b4d50e2de
kubsnn <51138889+kubsnn@users.noreply.github.com>
2023-11-21 17:32:09 +0100

    + initializer list for object type

---
+++ examples/example6/main.cpp
@@ -0,0 +1,51 @@
+﻿/*
+	This example shows usage of std::initializer_list constructor
+*/
+#include <iostream>
+#include "../../src/json.hpp"
+
+using json = jaszyk::json;
+
+int main()
+{
+	json data = {
+		{ "username", "kubsn" },
+		{ "friends", 
+			{
+				{ "name", "krabbens" }
+			}
+		},
+		{ "repos", json::array(
+			{
+				"json",
+				"consolecolors",
+				"tcp-chat"
+			})
+		},
+		{ "stars", 0 },
+		{ "forks", json::null() },
+		{ "is_admin", false }
+	};
+
+	std::cout << data.dump(true) << std::endl;
+}
+
+/*
+// OUTPUT:
+{
+	"username" : "kubsn",
+	"friends" : {
+		"name" : "krabbens"
+	},
+	"repos" : [
+		"json",
+		"consolecolors",
+		"tcp-chat"
+	],
+	"stars" : 0,
+	"forks" : null,
+	"is_admin" : false
+}
+	
+*/
+
